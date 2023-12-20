ebfa698eeec3f61ab58681c9f718a27ca880fe7c
arjunv075 <arjunv075@gmail.com>
2023-12-20 16:58:44 +0530

    add ex39.py

---
+++ ex39.py
@@ -0,0 +1,26 @@
+things = ['a', 'b', 'c', 'd']
+print(things[1])
+
+things[1] = 'z'
+print(things[1])
+things
+['a', 'z', 'c', 'd']
+
+stuff = {'name': 'Zed', 'age': 39, 'height': 6 * 12 + 2}
+print(stuff['name'])
+
+print(stuff['age'])
+print(stuff['height'])
+stuff['city'] = "SF"
+print(stuff['city']) 
+
+
+stuff[1] = "Wow"
+stuff[2] = "Neato"
+print(stuff[1])
+print(stuff[2])
+
+stuff.pop('city')
+stuff.pop(1)
+stuff.pop(2)
+{'name': 'Zed', 'age': 39, 'height': 74}
